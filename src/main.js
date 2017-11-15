040079a15ae718b83593c0d6bc4ce53e0f54bde6
Paul Glavich <glav@theglavs.com>
2017-11-16 10:38:48 +1100

    Few more bits from the intro tutorial

--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
-﻿import Vue from 'vue';
+﻿'use strict';
+import Vue from 'vue';
 
 var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!'
     }
-})
+});
+
+var test = new Vue({
+    el: '#test1',
+    data: {
+        items: [
+            'item1',
+            'this',
+            'that'
+        ],
+        label: 'click me'
+    },
+    methods: {
+        testmeth: function () {
+            alert('clicked');
+        }
+    }
+});
+
+var test2 = new Vue({
+    el: '#test2',
+    methods: {
+        toggle: function () {
+            this.showText = !this.showText;
+        }
+    },
+    data: {
+        showText: true
+    }
+});
