1c438aa456ac89b8947546286d53a6ff1655bf82
Paul Glavich <glav@theglavs.com>
2017-11-16 16:30:03 +1100

    A little more cleanup

--- src/main.js
+++ src/main.js
@@ -2,14 +2,6 @@
 
 import Vue from 'vue';
 import app from './components/app/app.vue';
-// import navheader from './components/header/navheader.vue'
-
-// var hdr = new Vue({
-//    el: '#navheader' ,
-//    template: '<navheader />',
-//    components: { navheader }
-
-// });
 
 var bootstrap = new Vue({
     el: '#app',
